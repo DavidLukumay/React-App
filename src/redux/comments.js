0dcc13140a31de0963a8d501027cbfeba930f35c
David Lukumay <lukumaydavid@gmail.com>
2019-08-01 01:16:08 +0300

    Fetch Handling Errors

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -2,15 +2,15 @@ import * as ActionTypes from './ActionTypes';
 
 
 export const Comments = (state = {
-        errmess: null,
+        errMess: null,
         comments: []
     }, action) => {
     switch(action.type){
         case ActionTypes.ADD_COMMENTS:
-            return {...state, isLoading: false, errmess: null, comments: action.payload};
+            return {...state, isLoading: false, errMess: null, comments: action.payload};
 
         case ActionTypes.COMMENTS_FAILED:
-            return {...state, isLoading: false, errmess: action.payload, comments: []};
+            return {...state, isLoading: false, errMess: action.payload, comments: []};
 
         case ActionTypes.ADD_COMMENT:
             var comment = action.payload;
--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -3,16 +3,16 @@ import * as ActionTypes from './ActionTypes';
 
 export const Dishes = (state = {
     isLoading: true,
-    errmess: null,
+    errMess: null,
     dishes: []
 }, action) => {
     switch(action.type){
         case ActionTypes.ADD_DISHES:
-            return {...state, isLoading: false, errmess: null, dishes: action.payload};
+            return {...state, isLoading: false, errMess: null, dishes: action.payload};
         case ActionTypes.DISHES_LOADING:
-            return {...state, isLoading: true, errmess: null, dishes: []};
+            return {...state, isLoading: true, errmMess: null, dishes: []};
         case ActionTypes.DISHES_FAILED:
-            return {...state, isLoading: false, errmess: action.payload, dishes: []};
+            return {...state, isLoading: false, errMess: action.payload, dishes: []};
         default:
             return state;
     }
